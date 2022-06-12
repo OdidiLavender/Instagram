b201475b99f16c65092611395aad246f292e91cb
OdidiLavender <lavender.odidi@student.moringaschool.com>
2022-06-12 10:26:26 +0300

    made changes

--- users/admin.py
+++ users/admin.py
@@ -12,13 +12,18 @@ from users.models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     """Profile Admin"""
-    # Lista de los atributos que mostrara en el admin
+
+
     list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
-    # Lista de links que llevan al detalle
+
+
+
     list_display_links = ('pk', 'user')
-    # Lista de editables in situ
+
+
     list_editable = ('phone_number', 'website', 'picture')
-    # Campos en los que se puede buscar
+
+    
     search_fields = (
         'user__email',
         'user__username',
@@ -26,26 +31,22 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__last_name',
         'phone_number'
     )
-    # Campos por los que se puede filtrar
     list_filter = (
         'user__is_active',
         'user__is_staff',
         'created',
         'modified',
     )
-    # Agrupar campos
+
     fieldsets = (
         (
             'Profile',
             {
-                # Para organizarlos horizontalmente se puede hacer
-                # colocando una tupla dentro de otra tupla y la coma
-                # (('user', 'picture'),)
+                
                 'fields': (('user', 'picture'),)
             }
         ),
-        # Si no queremos que aparezca
-        # la barra azul de titulo podemos pasarle None
+       
         (
             'Extra info',
             {
